6778fbf9f46399ff28b70913f7938c773e64a076
Brandon Fallin <Fallin_Brandon@bah.com>
2019-05-09 22:01:05 -0400

    Added SVM entry point

---
+++ svm/main.py
@@ -0,0 +1,10 @@
+"""
+This is the main entry point of the Support Vector Machine approach
+to classifying a poker hand
+"""
+
+def main():
+    print("hello world!")
+
+if __name__ == "__main__":
+    main()
