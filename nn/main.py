5af0119827cac9ced18db45f1dfcad5234a64ef6
Brandon Fallin <Fallin_Brandon@bah.com>
2019-05-09 22:00:52 -0400

    Added NN entry point

---
+++ nn/main.py
@@ -0,0 +1,10 @@
+"""
+This is the main entry point of the Neural Network approach
+to classifying a poker hand
+"""
+
+def main():
+    print("hello world!")
+
+if __name__ == "__main__":
+    main()
